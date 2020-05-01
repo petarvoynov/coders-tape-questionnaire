357f09dcbb810cebf034dcf6ae28e41ecd724402
Petar Voynov <ksihonm@abv.bg>
2020-05-01 16:00:27 +0300

    Added error messages in the create questionnaire form

--- resources/views/questionnaires/create.blade.php
+++ resources/views/questionnaires/create.blade.php
@@ -14,12 +14,18 @@
                             <label for="title">Title</label>
                             <input name="title" type="text" class="form-control" id="title" aria-describedby="titleHelp" placeholder="Enter Title">
                             <small id="titleHelp" class="form-text text-muted">Give your questionnaire a title that attracts attention.</small>
+                            @error('title')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
                         </div>
 
                         <div class="form-group">
                             <label for="purpose">Purpose</label>
                             <input name="purpose" type="text" class="form-control" id="purpose" aria-describedby="purposeHelp" placeholder="Enter Purpose">
                             <small id="purposeHelp" class="form-text text-muted">Giving a purpose will increase responses.</small>
+                            @error('title')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
                         </div>
 
                         <button type="submit" class="btn btn-primary">Create Questionnaire</button>

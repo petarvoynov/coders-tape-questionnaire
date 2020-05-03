c7605b8888b0e550c85bfccace71841acd18ea28
Petar Voynov <ksihonm@abv.bg>
2020-05-03 20:24:07 +0300

    Added the relationships between Questionnaire, Survey and SurveyResponse

--- app/Survey.php
+++ app/Survey.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     protected $guarded = [];
+
+    public function questionnaire()
+    {
+        return $this->belongsTo(Questionnaire::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(SurveyResponse::class);
+    }
 }
--- app/SurveyResponse.php
+++ app/SurveyResponse.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class SurveyResponse extends Model
 {
     protected $guarded = [];
+
+    public function survey()
+    {
+        return $this->belongsTo(Survey::class);
+    }
 }
